8b983406ec8c0ba65cabce7b2aae0cec759dcf20
liliana-jerez <lilianar.jerezg@gmail.com>
2020-12-10 16:05:58 -0500

    creation login

--- src/components/Forms/Login/LoginForm.js
+++ src/components/Forms/Login/LoginForm.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import {Button, Input} from '@material-ui/core';
+import logo from '../../../img/logo.jpg';
+
 
 const LoginForm = () =>{
+  
 
     return(
-        <div></div>
+         
+            <form>
+                <div>
+                    <img src={logo} width="200px" height="200px"></img>
+                    <h2 >Ingresa a tu cuenta</h2>
+                    <div>
+                       <label htmlFor="email">Correo Eletronico: </label>
+                       
+ 
+                    </div>
+                    <input
+                        type="email"
+                         name="email"
+                        id="email"
+                        ></input>
+                    <div className="form-group">
+                        <label htmlFor="password">Contraseña: </label>
+                        
+                    </div>
+                    <input 
+                         type="password" 
+                        name="password" 
+                        id="password" 
+                    ></input>
+                    <div>
+                        <Button variant="contained" color="primary">Ingresar</Button>
+                    </div>
+                </div>
+                
+                
+            </form>  
+     
+
+        
     )
 }
 export default LoginForm;
--- src/components/Screens/Login/index.js
+++ src/components/Screens/Login/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import LoginForm from '../Forms/LoginForm';
+import LoginForm from '../../Forms/Login/LoginForm';
+import { Card, CardContent } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+      minWidth: 275,
+      alignItems: "center",
+      padding: '50px 500px 20px 500px',
+      justify: "center",
+      display: "flex",
+          
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '5px',
+      transform: 'scale(0.8)',
+      
+    },
+    
+    
+  });
+     
 
 
 const Login = () =>{
+    const classes = useStyles();
     return(
-        <div>
-            <LoginForm />
+        <div className={classes.root} >
+          <Card>
+            <CardContent variant="outlined" >
+            <LoginForm  />
+            </CardContent>
+           
+          </Card>
         </div>
+        
     )
 }
 
